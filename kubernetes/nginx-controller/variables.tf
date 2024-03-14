7a8c3393d2175885eb6427ed3adac7b3ae5920ed
oluseyi bamgbose <134118317+obamstracon@users.noreply.github.com>
2024-03-13 20:04:25 -0700

    Update variables.tf

--- kubernetes/nginx-controller/variables.tf
+++ kubernetes/nginx-controller/variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
   description = "This is the domain name"
   type        = string
-  default     = "nwokolo.live"
+  default     = "digitalview74.online"
 }
   
 variable "alt_domain_name" {
   description = "This is the alternative domain name"
   type        = string
-  default     = "*.nwokolo.live"
+  default     = "*.digitalview74.online"
 }
